83bd35e02195c774234dbe503326a1a7afc09715
asim <asimbaidya.me@gmail.com>
2024-01-14 02:05:01 +0600

    chore: new goal to recreate the game with less overhead

---
+++ tetris-2.0.py
@@ -0,0 +1,7 @@
+# Rebuild the game but,
+# todo: remote unnecessary code
+# todo: bix bug(rotation issue of I block)
+# todo: add preview of next block
+# todo: live display of score
+# todo: remove OOP if possible
+# deadlines: 25/01/2024
